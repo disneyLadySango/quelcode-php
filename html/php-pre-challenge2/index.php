d8a16982fb6185f629eac8e69a67bbb755adaa83
disneyLadySango <sangota.d.duck@gmail.com>
2020-08-25 11:18:12 +0900

    mod: PHP練習課題２ ・バブルソートでの実装

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -1,11 +1,38 @@
 <?php
-$array = explode(',', $_GET['array']);
 
-// 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-
+/**
+ * パラメータからデータを取得する
+ * @param $key 対象のパラメータ
+ */
+function getResquestParam($key) {
+  return $_GET[$key];
 }
-// 修正はここまで
+
+/**
+ * パラメータによってソート条件を変更する
+ * @param $a 比較対象の値
+ * @param $b 比較対象の値
+ */
+function sortCondition($a, $b) {
+  $sort = filter_input(INPUT_GET, 'sort');  
+  if (is_null($sort)) {
+    $sort = "ASC";
+  }
+  return ($sort === "ASC") ? ($a > $b) : ($a < $b);
+}
+
+
+$array = explode(',', getResquestParam("array"));
+$arrayLength = count($array);
+for ($i = 0; $i < $arrayLength; $i++) {
+  for ($j = 1; $j < $arrayLength ; $j++ ) {
+    if(sortCondition($array[$j - 1], $array[$j])) {
+      $temp = $array[$j];
+      $array[$j] = $array[$j - 1];
+      $array[$j - 1] = $temp;
+    }
+  }
+}
 
 echo "<pre>";
 print_r($array);
